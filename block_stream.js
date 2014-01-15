fb632e081a5d54c36817e8815bbe4f4c3139820b
James Lal <james@lightsofapollo.com>
2014-01-15 12:29:00 -0800

    Use abstract class/stream for block_stream (and soon commit stream)

--- block_stream.js
+++ block_stream.js
@@ -1,35 +1,17 @@
-var stream = require('stream'),
-    azure = require('azure'),
-    assert = require('assert');
+var AzureStream = require('./azure_stream');
 
-/**
-Initialize a stream to upload content to block blob storage.
 
-@param {BlobService} service blob service from the azure module.
-@param {String} container to upload into.
-@param {String} path to upload to.
-@param {Object} [options] for the stream.
-*/
-function BlockStream(service, container, path, options) {
-  stream.Transform.call(this, options);
-
-  assert(service, 'service is required');
-  assert(container, 'must pass container');
-  assert(path, 'must provide path');
-
-  this.service = service;
-  this.container = container;
-  this.path = path;
-
+function BlockStream() {
+  AzureStream.apply(this, arguments);
 
   // incremented for each block pushed
   this._blockOffset = 0;
-
+  // the output is the block id (an object / not buffer)
   this._readableState.objectMode = true;
 }
 
 BlockStream.prototype = {
-  __proto__: stream.Transform.prototype,
+  __proto__: AzureStream.prototype,
 
   _transform: function(buffer, encoding, done) {
     var blockId = this.service.getBlockId(
